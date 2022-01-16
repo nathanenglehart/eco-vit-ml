6ba4ef1b720dd49d0b7d67ca738fbc2c363cb944
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2022-01-15 23:44:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import Ridge
+
+# Nathan Englehart, Ishaq Kothari, Raul Segredo
 
 if __name__ == "__main__":
-  mode = 1 # can set to 1, 2, 3
+
+	verbose = True
+	cv = True
+	mode = 1
+
+	dataset = pd.read_csv("data/SYB64_319_202110_Ratio of girls to boys in education.csv", encoding="ISO-8859-1")
+	dataset.columns = ['Region Code','Ratio of girls to boys','Year','Series','Value','Footnotes','Sources']
+
+	print(dataset)
+
+	if(mode == 1):
+
+		if(verbose):
+			print("mode 1: ridge regression")
+
+		model = Ridge(alpha=1.0)
+
+		t = np.array(dataset['Value'])
+		
+		X = np.array(dataset['Year'])
+		
+
+	
+	
+
