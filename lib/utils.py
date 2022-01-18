7fd6c2d401b7cfb8c113ac25e09a1340494b83ab
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2022-01-18 03:29:32 -0500

    lasso

--- lib/utils.py
+++ lib/utils.py
@@ -9,12 +9,6 @@ def load_dataset():
 
 	"""
 
-	lcb_dataset = pd.read_csv("data/LCB_ind_na.csv")
-	lcb_dataset.columns = ['code','iso','country','LCB.ind.1995','LCB.ind.1996','LCB.ind.1997','LCB.ind.1998','LCB.ind.1999','LCB.ind.2000','LCB.ind.2001','LCB.ind.2002','LCB.ind.2003','LCB.ind.2004','LCB.ind.2005','LCB.ind.2006','LCB.ind.2007','LCB.ind.2008','LCB.ind.2009','LCB.ind.2010','LCB.ind.2011','LCB.ind.2012','LCB.ind.2013','LCB.ind.2014','LCB.ind.2015','LCB.ind.2016','LCB.ind.2017','LCB.ind.2018','LCB.ind.2019','LCB.ind.2020']
-
-	cda_dataset = pd.read_csv("data/CDA_ind_na.csv")
-	cda_dataset.columns = ['code','iso','country','CDA.ind.1995','CDA.ind.1996','CDA.ind.1997','CDA.ind.1998','CDA.ind.1999','CDA.ind.2000','CDA.ind.2001','CDA.ind.2002','CDA.ind.2003','CDA.ind.2004','CDA.ind.2005','CDA.ind.2006','CDA.ind.2007','CDA.ind.2008','CDA.ind.2009','CDA.ind.2010','CDA.ind.2011','CDA.ind.2012','CDA.ind.2013','CDA.ind.2014','CDA.ind.2015','CDA.ind.2016','CDA.ind.2017','CDA.ind.2018','CDA.ind.2019','CDA.ind.2020']
-
 	grl_dataset = pd.read_csv("data/GRL_ind_na.csv")
 	grl_dataset.columns = ['code','iso','country','GRL.ind.1995','GRL.ind.1996','GRL.ind.1997','GRL.ind.1998','GRL.ind.1999','GRL.ind.2000','GRL.ind.2001','GRL.ind.2002','GRL.ind.2003','GRL.ind.2004','GRL.ind.2005','GRL.ind.2006','GRL.ind.2007','GRL.ind.2008','GRL.ind.2009','GRL.ind.2010','GRL.ind.2011','GRL.ind.2012','GRL.ind.2013','GRL.ind.2014','GRL.ind.2015','GRL.ind.2016','GRL.ind.2017','GRL.ind.2018','GRL.ind.2019','GRL.ind.2020']
 
@@ -24,27 +18,26 @@ def load_dataset():
 	wtl_dataset = pd.read_csv("data/WTL_ind_na.csv")
 	wtl_dataset.columns = ['code','iso','country','WTL.ind.1995','WTL.ind.1996','WTL.ind.1997','WTL.ind.1998','WTL.ind.1999','WTL.ind.2000','WTL.ind.2001','WTL.ind.2002','WTL.ind.2003','WTL.ind.2004','WTL.ind.2005','WTL.ind.2006','WTL.ind.2007','WTL.ind.2008','WTL.ind.2009','WTL.ind.2010','WTL.ind.2011','WTL.ind.2012','WTL.ind.2013','WTL.ind.2014','WTL.ind.2015','WTL.ind.2016','WTL.ind.2017','WTL.ind.2018','WTL.ind.2019','WTL.ind.2020']
 
-	del cda_dataset['code']
+	mld_dataset = pd.read_csv("data/mld_cleaned.csv")
+	mld_dataset.columns = ['code','iso','country','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
+
 	del grl_dataset['code']
 	del tcl_dataset['code']
 	del wtl_dataset['code']
 
-	del cda_dataset['iso']
 	del grl_dataset['iso']
 	del tcl_dataset['iso']
 	del wtl_dataset['iso']
 
-	del cda_dataset['country']
 	del grl_dataset['country']
 	del tcl_dataset['country']
 	del wtl_dataset['country']
+	
+	mld_dataset = pd.concat([mld_dataset,wtl_dataset],axis=1)
+	mld_dataset = pd.concat([mld_dataset,tcl_dataset],axis=1)
+	mld_dataset = pd.concat([mld_dataset,grl_dataset],axis=1)
 
-	lcb_dataset = pd.concat([lcb_dataset,cda_dataset],axis=1)
-	lcb_dataset = pd.concat([lcb_dataset,grl_dataset],axis=1)
-	lcb_dataset = pd.concat([lcb_dataset,tcl_dataset],axis=1)
-	lcb_dataset = pd.concat([lcb_dataset,wtl_dataset],axis=1)
-
-	return lcb_dataset
+	return mld_dataset
 
 def build_dataset(dataset,countries,verbose):
 	
@@ -111,24 +104,63 @@ def build_dataset(dataset,countries,verbose):
 					print(msg)
 					feature += 1
 
-				X.append(np.array(col)/np.linalg.norm(np.array(col)))
+				X.append(np.array(col))
 				col.clear()
 				col_count = 0
 
 		if(verbose):
 			print("")
-
+		
 		t = np.array(t)
 		X = np.array(X,dtype=object).T
-		country.append(entry[0]) # code
-		country.append(entry[1]) # iso
-		country.append(entry[2]) # country
-		country.append(t) # target vector
-		country.append(X) # train matrix
+		
+		check_t = True
+		check_X = True
+		
+		if((t.dtype.char == 'S') == False and np.isnan(np.sum(t)) == False):
+			check_t = True
+		else:
+			check_t = False
 
-		countries.append(np.array(country,dtype=object))
+		for i in range(len(X)):
+			
+			x = X[i]
+			
+			if((x.dtype.char == 'S') == True or np.isnan(np.sum(x)) == True):
+				check_X = False
+				break
+		
+		if(check_t == True and check_X == True):	
+			
+			country.append(entry[0]) # code
+			country.append(entry[1]) # iso
+			country.append(entry[2]) # country
+			country.append(t) # target vector
+			country.append(X) # train matrix
+
+			countries.append(np.array(country,dtype=object))
 
 		country.clear()
 
 	return np.array(countries)
 
+
+def build_world(countries):
+	
+	""" Returns averaged X matrix and t vector for all countries.
+
+		Args:
+			
+			countries::[Numpy array]
+				Array countaining X matrix and t vector for all loaded countries
+
+	"""
+
+	t = countries[0][3]
+	X = countries[0][4]
+
+	for i in range(len(countries)-1):
+		t = t + countries[i+1][3]
+		X = X + countries[i+1][4]
+	
+	return t / (len(countries)), X / (len(countries))
