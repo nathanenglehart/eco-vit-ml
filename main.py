3a2bf0ccf7ec5dc04b78ee60ae7107c52ce1ee4d
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2022-01-18 03:29:01 -0500

    lasso

--- main.py
+++ main.py
@@ -1,65 +1,25 @@
 import numpy as np
 import pandas as pd
-import warnings
 
 from matplotlib import pyplot as plt
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import Lasso
+
 from sklearn.neural_network import MLPClassifier
 
+from lib.reg import lasso_regression
+from lib.utils import build_world
 from lib.utils import build_dataset
 from lib.utils import load_dataset
 from lib.kfcv import lasso_kfcv
 
 # Nathan Englehart, Ishaq Kothari, Raul Segredo (Autumn 2021)
 
-def lasso_regression(countries,lam,degree,country_idx):
-	
-	""" Runs lasso regression with the given parameters and returns predictions corresponding to years.
 
-		Args:
-
-			countries::[Numpy Array]	
-				Numpy array containing iso, code, name, t, and X info for each selected country
-
-			lam::[Float]
-				Weight penalty for high polynomial degrees
-
-			degree::[Integer]
-				Polynomial degree to use with lasso regression
-
-			country_idx::[Integer]
-				Index of country for which we want to compute a regression for
-			
-	"""
-	
-	warnings.filterwarnings('ignore')
-	model=make_pipeline(PolynomialFeatures(degree),Lasso(alpha=lam,max_iter=10000, tol=1e-2))
-	model.fit(countries[country_idx][4],countries[country_idx][3]) # X, t
-
-	year = 1995
-
-	preds = list()
-	years = list()
-		
-	for i in range(26):
-		years.append(year)
-		year_idx = year - 1995
-		year += 1
-		preds.append(model.predict([countries[country_idx][4][year_idx]]))
-
-	preds = np.array(preds)
-	years = np.array(years)
-
-	return years, preds
-
-def grid_search(cv,lams,degrees,countries,country_idx,seed,k,verbose):
+def grid_search(cv,lams,degrees,data,seed,k,verbose):
 
 	""" Calculates and returns the optimal D and lambda parameters after running grid search on given model.
 
 		Args:
-			
+
 			cv::[Function]
 				Cross validation function
 
@@ -68,12 +28,9 @@ def grid_search(cv,lams,degrees,countries,country_idx,seed,k,verbose):
 
 			degrees::[List]
 				List of polynomial degrees to test
-				
-			countries::[Numpy Array]
-				Mutlidimensional numpy array to plug into regression algorithm
-		
-			country_idx::[Integer]
-				Index of country to run regression on
+
+			data::[Numpy Array]
+				Array holding iso, name, code, t vector, and X matrix 
 
 			seed::[Integer]
 				Seed for random number generation used in cross validation algorithm
@@ -86,8 +43,8 @@ def grid_search(cv,lams,degrees,countries,country_idx,seed,k,verbose):
 
 	"""
 
-	min_mse = lasso_kfcv(lasso_regression,countries,country_idx,k,seed,lams[0],1,verbose)
-	pair = 1, lams[0]
+	min_mse = cv(lasso_regression,data,k,seed,lams[0],degrees[0],verbose)
+	pair = degrees[0], lams[0]
 
 	# find every combination of lambda and D from our given lists
 	# if the average mse of a calculation is lower than our default, set it as our global min value
@@ -96,14 +53,13 @@ def grid_search(cv,lams,degrees,countries,country_idx,seed,k,verbose):
 	for lam in lams:
 
 		for degree in degrees:
-			
-			average_mse = cv(lasso_regression,countries,country_idx,k,seed,lam,degree,verbose)
-			
+
+			average_mse = cv(lasso_regression,data,k,seed,lam,degree,verbose)
+
 			if(average_mse < min_mse):
 				pair = degree, lam
-	
-	return pair
 
+	return pair
 
 def driver(verbose,mode,country_names,country_indicies,seed,k):
 
@@ -129,13 +85,13 @@ def driver(verbose,mode,country_names,country_indicies,seed,k):
 			k::[Integer]
 				Number of folds to split the data into
 
-	"""	
+	"""
 
 	# Create single dataset
 
 	dataset = load_dataset()
 	countries = build_dataset(dataset,country_names,verbose)
-	
+
 	# indexing works as follows
 	# countries[0] gives the first country from our list of selected countries in alphabetical order
 	# e.g. if our list contains Australia, Brazil, and Columbia, countries[0] would give Australia
@@ -154,62 +110,65 @@ def driver(verbose,mode,country_names,country_indicies,seed,k):
 	#	print("len(x3):",len(countries[0][4][:,2])) # tcl (tree cover)
 	#	print("len(x4):",len(countries[0][4][:,3])) # wtl (wetland)
 
+	# for our purposes, we actually want to consolidate all of this data into 
+	# a singular t vector and X matrix for the world
+
+	t, X = build_world(countries)
+	data = np.array(['0','world','WOR',t,X],dtype=object)
+
 	if(mode == 1):
 
 		if(verbose):
 			print("mode 1: lasso regression\n")
 
+
+
+		#lam = 1.0
+		#D = 3
+
+		#years, preds = lasso_regression(data,lam,D)
+
+
+
+
 		lams = [0.001,0.01,0.1,1.0,10.0]
 		degrees = [1,2,3,4,5,6,7]
 
-		for country_idx in country_indicies:
-		
-			# for each country under consideration
-			# run grid search to determine optimal lambda and D values
-			# display optimal values to user
-			# then use findings in lasso regression and plot
-
-			optimal_polynomial_order, optimal_weight_value = grid_search(lasso_kfcv,lams,degrees,countries,country_idx,seed,k,verbose)
-			D = optimal_polynomial_order
-			lam = optimal_weight_value
+		optimal_polynomial_order, optimal_weight_value = grid_search(lasso_kfcv,lams,degrees,data,seed,k,verbose)
+		D = optimal_polynomial_order
+		lam = optimal_weight_value
 			
-			print(country_names[country_idx])
-			print("optimal D:",D)
-			print("optimal lambda:",lam,"\n")
+		print("optimal D:",D)
+		print("optimal lambda:",lam,"\n")
 
-			years, preds = lasso_regression(countries,lam,D,country_idx)
-			plt.scatter(years, countries[country_idx][3], color = 'g')
-			plt.plot(years, preds, label="preds")
-			plt.title(country_names[country_idx])
-			plt.xlabel('Years')
-			plt.ylabel('LCB')
-			plt.show()
+		years, preds = lasso_regression(data,lam,D)
+		plt.scatter(years, data[3], color = 'g')
+		plt.plot(years, preds, label="preds")
+		plt.xlabel('Years')
+		plt.ylabel('MLD')
+		plt.show()
 
 	if(mode == 2):
-		
+
 		if(verbose):
 			print("mode 2: neural networks\n")
 
-
-
 if __name__ == "__main__":
-	
+
 	# verbose option indicates whether to run the program with verbose
-	# mode indicates which method to run the program with 
-	# i.e. 1 -> lasso regression, 2 -> neural networks 
+	# mode indicates which method to run the program with
+	# i.e. 1 -> lasso regression, 2 -> neural networks
 	# countries corresponds to each individual country to consider with our algorithms
 	# seed is used in random number generation and should be set to make results reproducable
-	# k is the number of folds to use when cross validating our algorithm to find 
-	# optimal parameters 
-	
+	# k is the number of folds to use when cross validating our algorithm to find
+	# optimal parameters
+
 	verbose = True
-	mode = 1 	
-	countries = np.array(['Nigeria','Zambia'])
+	mode = 1
+	#countries = np.array(['Nigeria','Zambia'])
+	countries = np.array(['Afghanistan','Albania','Algeria','Angola','Antigua and Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei Darussalam','Bulgaria','Burkina Faso','Burundi','Cabo Verde','Cambodia','Cameroon','Canada','Central African Republic','Chad','Chile','China','Colombia','Comoros','Costa Rica',"Cote d'Ivoire",'Croatia','Cuba','Cyprus','Czech Republic','Dem. Rep. Congo','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Eswatini','Ethiopia','Fiji','Finland','France','Gabon','Gambia','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hong Kong','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Lithuania','Luxembourg','Macao','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Micronesia','Moldova','Mongolia','Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua','Niger','Nigeria','North Macedonia','Norway','Oman','Pakistan','Palau','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Republic of Congo','Romania','Russia','Rwanda','Saint Kitts and Nevis','Saint Lucia','Saint Vincent and the Grenadines','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','South Africa','South Korea','South Sudan','Spain','Sri Lanka','Sudan','Suriname','Sweden','Switzerland','Taiwan','Tajikistan','Tanzania','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States of America','Uruguay','Uzbekistan','Vanuatu','Venezuela','Viet Nam','Yemen','Zambia','Zimbabwe'])
 	country_indicies = np.array([0])
 	seed = 40
 	k = 10
 
 	driver(verbose,mode,countries,country_indicies,seed,k)
-
-
-	
